1a2e34c2196c3011a29b72ea3d1cafe02de73d5c
Joonas Palosuo <joonas.palosuo@gmail.com>
2024-10-29 08:28:53 +0000

    initial commit

---
+++ eslint.config.js
@@ -0,0 +1,12 @@
+import eslintPluginAstro from 'eslint-plugin-astro';
+export default [
+  // add more generic rule sets here, such as:
+  // js.configs.recommended,
+  ...eslintPluginAstro.configs.recommended,
+  ...eslintPluginAstro.configs['jsx-a11y-strict'],
+  {
+    rules: {
+      "astro/jsx-a11y/no-redundant-roles": "off"
+    }
+  }
+];
