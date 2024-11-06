87e92538c16aa0ddaa1ae1862bba7fe0936dfdf9
Joonas Palosuo <joonas.palosuo@gmail.com>
2024-11-06 08:38:24 +0000

    feat: smoke tests to be ran after deployment

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,54 +1,54 @@
-import { defineConfig, devices } from '@playwright/test';
+import { defineConfig, devices } from "@playwright/test";
 
 /**
  * See https://playwright.dev/docs/test-configuration.
  */
 export default defineConfig({
-  testDir: './tests',
+  testDir: "./tests",
   fullyParallel: true,
   forbidOnly: !!process.env.CI,
   retries: process.env.CI ? 2 : 0,
-  workers: process.env.CI ? 1 : undefined as any,
-  reporter: 'html',
+  workers: process.env.CI ? 1 : (undefined as any),
+  reporter: "html",
   use: {
-    baseURL: 'http://127.0.0.1:4321',
+    baseURL: process.env.PLAYWRIGHT_TEST_BASE_URL ?? "http://127.0.0.1:4321",
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
-    trace: 'on-first-retry',
+    trace: "on-first-retry",
   },
 
   /* Configure projects for major browsers */
   projects: [
     {
-      name: 'chromium',
-      use: { ...devices['Desktop Chrome'] },
+      name: "chromium",
+      use: { ...devices["Desktop Chrome"] },
     },
 
     {
-      name: 'firefox',
-      use: { ...devices['Desktop Firefox'] },
+      name: "firefox",
+      use: { ...devices["Desktop Firefox"] },
     },
 
     {
-      name: 'webkit',
-      use: { ...devices['Desktop Safari'] },
+      name: "webkit",
+      use: { ...devices["Desktop Safari"] },
     },
 
     /* Test against mobile viewports. */
     {
-      name: 'android',
-      use: { ...devices['Pixel 5'] },
+      name: "android",
+      use: { ...devices["Pixel 5"] },
     },
     {
-      name: 'ios',
-      use: { ...devices['iPhone 12'] },
+      name: "ios",
+      use: { ...devices["iPhone 12"] },
     },
   ],
 
   /* Run your local dev server before starting the tests */
   webServer: {
-    command: 'npm run dev',
-    url: 'http://127.0.0.1:4321',
+    command: "npm run dev",
+    url: "http://127.0.0.1:4321",
     reuseExistingServer: !process.env.CI,
   },
 });
